aa84dbb193ed7eb742f8cccb0b5151bba9ca1ce9
graNdela <l.grandela@gmail.com>
2025-06-30 16:45:50 +0200

    creadas todas las salidas y casi terminado

--- recetario.py
+++ recetario.py
@@ -9,12 +9,13 @@ estas opciones que tenemos aquí:
 #*******************************************************************************************************#
 'USAREMOS ESTA RUTA COMO PRINCIPAL QUE ES LA CARPETA "Recetas" DONDE SE ENCUENTRAN LAS CATEGORIAS'
 ruta_principal = Path(Path.home(),'Proyecto-Recetario','Recetas')
-#.glob lo utilizamos para localizar todos los archivos .txt que serían las recetas en sí.
-recetitas = [numero.stem for numero in Path(ruta_principal).glob('**/*.txt')]
-numero_recetas = len(recetitas)
+
 # Tenemos que darle la bienvenida al usuario e indicarle la ruta de acceso al directorio donde se encuentra nuestra carpeta de recetas
 # Tambien le va a decir cuantas recetas en total tiene dicha carpeta.
 def bienvenida():
+    #.glob lo utilizamos para localizar todos los archivos .txt que serían las recetas en sí.
+    recetitas = [numero.stem for numero in Path(ruta_principal).glob('**/*.txt')]
+    numero_recetas = len(recetitas)
     print("-----------------------------------------------------")
     print('Bienvenido al Recetario. ¿En qué le podemos ayudar?: ')
     print("-----------------------------------------------------")
@@ -42,15 +43,24 @@ def elegir_categoria():
     # y seguidamente le pedimos al usuario que elija una de las categorias
     eleccion = input("¿Qué categoría elije?:\t")
     preguntar = True
+    contador = 0
     while preguntar == True:
+        contador += 1
         if eleccion.capitalize() in lista_categorias:
             preguntar = False
             return eleccion
+        elif contador%2 == 0:
+            print('\nTambién puede volver al inicio escribiendo "Salir"')
         else:
             print("Por favor, elija una categoría correcta y escríbala tal y como aparece en pantalla.\n")
             for categoria in categorias.iterdir():
                 print(categoria.name)
             eleccion = input("¿Qué categoría elije?:\t")
+            salida = eleccion.capitalize()
+            if salida == 'Salir':
+                preguntar = False
+                return salida
+    
             
 
 def leer_receta(eleccion):
@@ -62,17 +72,21 @@ def leer_receta(eleccion):
     print(f'\n¿Qué receta quiere leer en concreto?: ')
     elegir = input("(RECUERDO, escriba la receta tal y como aparece en pantalla)\nElija qué receta quiere leer:  ")
     while elegir not in recetas:
+        print('Cuando quiera volver al inicio escriba "Salir".')
         print('Por favor, escriba bien la receta')
         print(recetas)
         elegir = input("¿Qué receta quiere leer en concreto?: ")
-    receta = Path(ruta_recetas,(elegir+'.txt'))
-    system('cls')
-    print(f'Aquí tiene su receta de {elegir}:\n\n{receta.read_text()}\n\n') 
-    salir = True
-    while salir:
-        salida = input('Cuando quiera volver al inicio escriba aquí "Salir".\t').capitalize()
-        if salida == 'Salir':
-            salir = False
+        if elegir.capitalize() == 'Salir':
+            break
+    if elegir in recetas:    
+        receta = Path(ruta_recetas,(elegir+'.txt'))
+        system('cls')
+        print(f'Aquí tiene su receta de {elegir}:\n\n{receta.read_text()}\n\n') 
+        salir = True
+        while salir:
+            salida = input('Cuando quiera volver al inicio escriba aquí "Salir".\t').capitalize()
+            if salida == 'Salir':
+                salir = False
        
 '''2. En la opción 2 también se le va a hacer elegir una categoría, pero luego le va a pedir que
 escriba el nombre y el contenido de la nueva receta que quiere crear, y el programa va
@@ -104,12 +118,12 @@ def eliminar_receta(eleccion):
     print(recetas)
     eliminar = 'no'
     while eliminar == 'no':
+        print('Si no quiere eliminar ninguna receta escriba "Salir".\n')
         print(f'\n¿Qué receta quiere eliminar en concreto?: ')
         elegir = input("(RECUERDO, escriba la receta tal y como aparece en pantalla)\nElija qué receta quiere eliminar:  ")
         if elegir.capitalize() == 'Salir':
             break
         while elegir not in recetas:
-            if elegir not in recetas:
                 print('Por favor, escriba bien la receta')
                 print(recetas)
                 elegir = input("¿Qué receta quiere eliminar en concreto?: ")
@@ -122,7 +136,7 @@ def eliminar_receta(eleccion):
             eliminar = 'si'
         elif eliminar == 'no':
             system('cls')
-            print('Si no quiere eliminar ninguna receta escriba "Salir".')
+
         
     # print(f'¿Está seguro que quiere eliminar la receta {elegir}?:\n\n{receta.read_text()}\n\n') 
     # Path.unlink(missing_ok=False) esto como idea para eliminar una receta
@@ -141,10 +155,14 @@ def eliminar_categoria():
             remove_ruta.rmdir()
             preguntar = False
         else:
+            print('\nTambién puede volver al inicio escribiendo "Salir"\n')
             print("Por favor, elija una categoría correcta y escríbala tal y como aparece en pantalla.\n")
             for categoria in categorias.iterdir():
                 print(categoria.name)
-            eleccion = input("¿Qué categoría elije?:\t")
+            eleccion = input("¿Qué categoría elije?:\t")            
+            salida = eleccion.capitalize()
+            if salida == 'Salir':
+                break
     
     '6. Finalmente, la opción 6 simplemente va a finalizar la ejecución del código.'
 #esto lo tenemos dentro de la funcion opciones() para que finalice la ejecución del código      
@@ -153,15 +171,18 @@ def eliminar_categoria():
 def opciones(num):
     if num == 1:
         eleccion_usuario = elegir_categoria()
-        leer_receta(eleccion_usuario)
+        if eleccion_usuario.capitalize() != 'Salir':
+            leer_receta(eleccion_usuario)
     elif num == 2:
         eleccion_usuario = elegir_categoria()
-        crear_receta(eleccion_usuario)
+        if eleccion_usuario.capitalize() != 'Salir':
+            crear_receta(eleccion_usuario)
     elif num == 3:
         crear_categoria()
     elif num == 4:
         eleccion_usuario = elegir_categoria()
-        eliminar_receta(eleccion_usuario)
+        if eleccion_usuario.capitalize() != 'Salir':
+            eliminar_receta(eleccion_usuario)
     elif num == 5:
         eliminar_categoria()
     elif num == 6:
@@ -176,6 +197,7 @@ def opciones(num):
 
 finalizar = True
 while finalizar:
+    system('cls')
     bienvenida()
     numero = int(input("¿qué número elije?:  "))
     print("\n")
